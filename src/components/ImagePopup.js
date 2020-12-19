acc345760ebad52c306f3f0c32a30706b6edba09
Danila Engel <engeldanila@mail.ru>
2020-12-19 19:45:49 +0300

    fixed after review2

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -5,7 +5,7 @@ function ImagePopup({card, onClose}){
             <div className={`popup popup_image ${card ? "popup_opened" : ""}`}>
             <div className="popup__overlay popup__overlay-image">
             <div className="popup__content">
-              <img className="popup__image_image" src={card?.src} alt="картинка места"  />
+              <img className="popup__image_image" src={card.link} alt="картинка места"  />
               <p className="popup__caption_image"></p>
               <img className="popup__close-button popup__close-button_image" src={closeBtn} alt="кнопка выхода" onClick={onClose} />
             </div>
